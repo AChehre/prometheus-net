91e37a2c743b5b3f911980389b5fecff831eb018
ahmad <achehre@yahoo.com>
2020-08-05 13:29:08 +0430

    add route parameter to http client asp.net core

---
+++ Prometheus.HttpClient.AspNetCore/HttpClientMetrics/InProgressHttpClientParameterMapping.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Prometheus.HttpClientMetrics
+{
+    public class RouteHttpClientParameterMapping
+    {
+        public HttpClientParameterMapping Create()
+        {
+
+            var parameterName = "route";
+            var labelName = "route";
+            Func<HttpRequestMessage, string, string> labelValue = (HttpRequestMessage request, string parameterName) =>
+            {
+                if (parameterName == "route")
+                {
+
+
+                }
+                return string.Empty;
+            };
+
+            var httpParameter =  new HttpClientParameterMapping(parameterName,labelName, labelValue);
+
+            return httpParameter;
+
+        }
+    }
+}
